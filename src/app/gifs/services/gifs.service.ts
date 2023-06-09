35b3d8d5e03a4ee5fe0a878c7c9f7c4c45a4a0f3
Martín <magadiflo@gmail.com>
2023-06-08 23:06:46 -0500

    Leer del localStorage

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -18,14 +18,17 @@ export class GifsService {
     return [...this._tagHistory];
   }
 
-  constructor(private _http: HttpClient) { }
+  constructor(private _http: HttpClient) {
+    this._loadLocalStorage();
+    this._searchFirstElement();
+  }
 
   searchTag(tag: string): void {
     if (this._isInvalid(tag)) return;
     this._organizeHistory(tag.trim().toLowerCase());
 
     const params = new HttpParams()
-      .set("api_key", "3ZFhGljhvA8ctX3W29sNy5xCrjAqBwLE")
+      .set("api_key", GIPHY_API_KEY)
       .set("q", tag)
       .set("limit", 10);
 
@@ -56,5 +59,16 @@ export class GifsService {
 
   private _saveLocalStogare(): void {
     localStorage.setItem('history', JSON.stringify(this._tagHistory));
+  }
+
+  private _loadLocalStorage(): void {
+    this._tagHistory = localStorage.getItem('history') ? JSON.parse(localStorage.getItem('history')!) : [];
+  }
+
+  private _searchFirstElement(): void {
+    const tag = this._tagHistory.length > 0 ? this._tagHistory[0] : null;
+    if (tag) {
+      this.searchTag(tag)
+    }
   }
 }
