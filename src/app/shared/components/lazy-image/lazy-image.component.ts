bedd525259c1e1afaa544eff5ed23edb39e2af19
Martín <magadiflo@gmail.com>
2023-06-09 23:19:18 -0500

    Lazy Image - Parte 2

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-lazy-image',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./lazy-image.component.scss']
 })
 export class LazyImageComponent {
+
+  @Input({ alias: 'url', required: true }) url!: string;
+  @Input({ alias: 'alt' }) alt: string = 'No Title!';
+
+  public hasLoaded: boolean = false;
+
+  onLoad(): void {
+    console.log('Image loaded: ' + this.alt);
+    this.hasLoaded = true;
+  }
 
 }
